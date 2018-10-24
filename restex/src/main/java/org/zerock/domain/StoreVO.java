c9ec394b0dbd23821dbbb96d7a04b3922f7040f1
khj <gonnys520@gmail.com>
2018-10-24 18:56:49 +0900

    dsfsdfsf

---
+++ restex/src/main/java/org/zerock/domain/StoreVO.java
@@ -0,0 +1,13 @@
+package org.zerock.domain;
+
+import lombok.Data;
+
+@Data 
+public class StoreVO {
+	
+	private Integer sno;
+	private String sname;
+	private double lat, lng;
+	private String menu;
+
+}
